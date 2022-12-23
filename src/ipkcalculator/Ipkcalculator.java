fafdef1ed910a6b00eccebd406d70744119c5bc8
Ahlijati Nuraminah <ahlijati.nuraminah@gmail.com>
2022-12-23 09:00:55 +0700

    initial commit

---
+++ src/ipkcalculator/Ipkcalculator.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipkcalculator;
+
+/**
+ *
+ * @author zhiro
+ */
+public class Ipkcalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       halamanawal ha = new halamanawal();
+       ha.setVisible(true);
+       ha.setLocationRelativeTo(null);
+    }
+    
+}
